c92b7415bfce529aabac6d55db794794730aa9d1
guildfiction <xvinicius.bt94@gmail.com>
2026-07-30 10:22:12 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,8 @@
+/**
+ * INICIALIZAÇÃO DO CLIENTE SUPABASE
+ */
 const SUPABASE_URL = 'https://wluwbgyskmopzlhvqjee.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_nfX0TkWB6xaAtugr_Zv8Mg_728zP2Yc';
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// Cria a instância usando um nome único para não dar conflito de variável
+window.supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
