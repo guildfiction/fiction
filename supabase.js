7e8abbf4b6ba3cc7d6d204a07ae3d90c5ae9eb53
guildfiction <xvinicius.bt94@gmail.com>
2026-07-30 10:03:59 -0300

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://wluwbgyskmopzlhvqjee.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_nfX0TkWB6xaAtugr_Zv8Mg_728zP2Yc';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
